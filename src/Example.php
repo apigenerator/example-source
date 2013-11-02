24cc4a64e04e32921ff50542208532e84cc3e1bc
Tristan Lins <tristan.lins@bit3.de>
2013-11-02 18:01:53 +0100

    Add example class.

---
+++ src/Example.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * ApiGenerator.org
+ * Copyright (C) 2013 Tristan Lins
+ *
+ * PHP version 5
+ *
+ * @copyright  bit3 UG 2013
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @package    apigenerator.org
+ * @license    LGPL-3.0+
+ * @filesource
+ */
+
+/**
+ * This is an example class.
+ */
+class Example
+{
+	/**
+	 * Return the input value $bar.
+	 *
+	 * @param string $bar
+	 *
+	 * @return string
+	 */
+	public function foo($bar = 'zap')
+	{
+		return $bar;
+	}
+}
